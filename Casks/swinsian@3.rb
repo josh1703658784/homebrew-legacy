5161b5eee59f2d35426ebe1e2dbb318444a1cf99
josh_1752185424 <josh_1752185424@noreply.codeberg.org>
2025-08-19 11:48:56 -0700

    swinsian 3

---
+++ Casks/swinsian@3.rb
@@ -0,0 +1,27 @@
+cask "swinsian@3" do
+  version "3.0.0"
+  sha256 "0d178cf77177e37c5bb46b924d9b9f41f9efaa2ffaa6d2844d2af714f3e4d2fc"
+
+  url "https://www.swinsian.com/sparkle/Swinsian_#{version}.zip"
+  name "Swinsian"
+  desc "Music player"
+  homepage "https://swinsian.com/"
+
+  livecheck do
+    url "https://www.swinsian.com/sparkle/sparklecast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+
+  app "Swinsian.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.swinsian.SwinsianArtworkTagReader",
+    "~/Library/Application Scripts/com.swinsian.SwinsianChapterReader",
+    "~/Library/Application Support/Swinsian",
+    "~/Library/Containers/com.swinsian.SwinsianArtworkTagReader",
+    "~/Library/Containers/com.swinsian.SwinsianChapterReader",
+    "~/Library/Preferences/com.swinsian.Swinsian.plist",
+  ]
+end
